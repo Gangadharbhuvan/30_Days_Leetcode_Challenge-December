d56af54e8c440308652c4df8efb5816dad2d4f69
Gangadhar DN <44730031+Gangadharbhuvan@users.noreply.github.com>
2020-12-24 22:41:05 +0530

    Added Day-24 Solution

---
+++ 24.Swap_Nodes_in_Pairs/24.Swap_Nodes_in_Pairs.py
@@ -0,0 +1,46 @@
+'''
+	Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes. Only nodes itself may be changed.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+Example 2:
+
+Input: head = []
+Output: []
+Example 3:
+
+Input: head = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 100].
+0 <= Node.val <= 100
+
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head):
+        dummy = ListNode(-1)
+        dummy.next = head
+        pre = dummy
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+            pre.next, b.next, a.next = b, a, b.next
+            pre = a
+        return dummy.next
