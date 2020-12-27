e24cf55231ceda5895e7248cc25afc7bad121c9b
Gangadhar DN <44730031+Gangadharbhuvan@users.noreply.github.com>
2020-12-27 19:00:25 +0530

    Added Day-27 Solution

---
+++ 27.Jump_Game_IV/27.Jump_Game_IV.py
@@ -0,0 +1,103 @@
+'''
+    Given an array of integers arr, you are initially positioned at the first index of the array.
+
+In one step you can jump from index i to index:
+
+i + 1 where: i + 1 < arr.length.
+i - 1 where: i - 1 >= 0.
+j where: arr[i] == arr[j] and i != j.
+Return the minimum number of steps to reach the last index of the array.
+
+Notice that you can not jump outside of the array at any time.
+
+ 
+
+Example 1:
+
+Input: arr = [100,-23,-23,404,100,23,23,23,3,404]
+Output: 3
+Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array.
+Example 2:
+
+Input: arr = [7]
+Output: 0
+Explanation: Start index is the last index. You don't need to jump.
+Example 3:
+
+Input: arr = [7,6,9,6,9,6,9,7]
+Output: 1
+Explanation: You can jump directly from index 0 to index 7 which is last index of the array.
+Example 4:
+
+Input: arr = [6,1,9]
+Output: 2
+Example 5:
+
+Input: arr = [11,22,7,7,7,7,7,7,7,22,13]
+Output: 3
+ 
+
+Constraints:
+
+1 <= arr.length <= 5 * 10^4
+-10^8 <= arr[i] <= 10^8
+   Hide Hint #1  
+Build a graph of n nodes where nodes are the indices of the array and edges for node i are nodes i+1, i-1, j where arr[i] == arr[j].
+   Hide Hint #2  
+Start bfs from node 0 and keep distance, answer is the distance when you reach onode n-1.
+
+
+'''
+
+class Solution:
+    def minJumps(self, arr) -> int:
+        n = len(arr)
+        if n <= 1:
+            return 0
+
+        graph = {}
+        for i in range(n):
+            if arr[i] in graph:
+                graph[arr[i]].append(i)
+            else:
+                graph[arr[i]] = [i]
+
+        curs = [0]  # store layers from start
+        visited = {0, n-1}
+        step = 0
+
+        other = [n-1] # store layers from end
+
+        # when current layer exists
+        while curs:
+            # search from the side with fewer nodes
+            if len(curs) > len(other):
+                curs, other = other, curs
+            nex = []
+
+            # iterate the layer
+            for node in curs:
+
+                # check same value
+                for child in graph[arr[node]]:
+                    if child in other:
+                        return step + 1
+                    if child not in visited:
+                        visited.add(child)
+                        nex.append(child)
+
+                # clear the list to prevent redundant search
+                graph[arr[node]].clear()
+
+                # check neighbors
+                for child in [node-1, node+1]:
+                    if child in other:
+                        return step + 1
+                    if 0 <= child < len(arr) and child not in visited:
+                        visited.add(child)
+                        nex.append(child)
+
+            curs = nex
+            step += 1
+
+        return -1
